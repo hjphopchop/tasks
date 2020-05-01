1f483ac2aff49a0cbabf8a5dfe6a5d140eb83915
Igor Parfyonov <1parfenov1work@gmail.com>
2020-05-01 22:21:17 +0300

    Hello boys

--- array+object.js
+++ array+object.js
@@ -47,3 +47,51 @@ alert(obj["6"]); */
  
  let day = prompt("What is day today? (number)" , "");
  alert(obj[day]); */
+/*  let str = "Прохожу курс на #coursera по #javascript";
+ let str2 = str.split(" ");
+ let i = 0;
+ let str3 = [];
+
+ while ( i < str2.length) {
+     if (str2 [i] [0] =="#") {
+            str3.push(str2[i]);
+     }
+     
+     i++;
+ }
+let str4 = str3.join("");
+let str5 = str4.split("#");
+str5.splice(0, 1);
+alert(str);
+alert(str2);
+alert(str3);
+alert(str4);
+alert(str5);
+alert(str5[0]);
+ */
+
+function century(year) {
+    let centur = +year / 100;
+    if (centur % 100 != 0)
+    {
+        centur++;
+    }
+    return Math.floor(centur);
+   /*   if (year.length <=2) {
+      return 1;
+      
+    }
+     let ch = [year.length - 1];
+      if (year.charAt(ch) >=1 && year.length >2) {
+      return (+year.substring(0, year.length-2) + 1);
+    } 
+     else {
+      return +year.substring(0, year.length -2);
+  }   
+   */
+ /* let ch = [year.length - 1];
+ 
+ return (year.charAt(ch)); */
+  }
+
+  alert(century("540980"));
